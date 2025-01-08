6e12cb9ffd40b68604e7f9438c6f7268f2b0fd34
Zeepheru <59129423+Zeepheru@users.noreply.github.com>
2025-01-09 03:30:22 +0800

    Remove unnecessary variable

--- cidderbot/utils/string_utils/string_exceptions.py
+++ cidderbot/utils/string_utils/string_exceptions.py
@@ -1,5 +1,5 @@
 class InvalidAnsiColorCodeException(Exception):
-    def __init__(self, message, errors):
+    def __init__(self, message):
         # Call the base class constructor with the parameters it needs
         if message:
             super().__init__("Supplied ANSI color code is incorrect: " + message)
